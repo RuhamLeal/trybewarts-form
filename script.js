f2706c4c5f43cf617a8f0dd8333914905aa2240c
Ruham Leal <ruhamxlpro@hotmail.com>
2022-06-07 16:13:35 -0300

    Adiciona requisito 3

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputEmail = document.querySelector('#input-email');
+const inputSenha = document.querySelector('#input-senha');
+const botaoEntrar = document.querySelector('#botao-entrar');
+
+function verificarInputValues() {
+  if (inputEmail.value !== 'tryber@teste.com' && inputSenha.value !== '123456') {
+    alert('Email ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
+}
+
+botaoEntrar.addEventListener('click', verificarInputValues);
